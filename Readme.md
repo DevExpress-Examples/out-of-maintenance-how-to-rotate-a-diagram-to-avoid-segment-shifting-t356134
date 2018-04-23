44c465f91684aef9de40549d22cd8d1d2cbf7514
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to rotate a diagram to avoid segment shifting 
+
+
+<p>If a series point value is changed, you may wish to keep an old start segment position. It is possible to use the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsPieSeries2D_Rotationtopic">PieSeries2D.Rotation</a> property for this task. </p>
+
+<br/>
+
+
---
+++ VB/ChartSample/MainWindow.xaml.vb
@@ -0,0 +1,45 @@
+﻿Imports System
+Imports System.Linq
+Imports System.Windows
+Imports System.Windows.Input
+Imports DevExpress.Xpf.Charts
+Imports System.Collections.Generic
+
+Namespace ChartSample
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+
+        End Sub
+        Private Sub Chart_MouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            Dim chartControl As ChartControl = DirectCast(sender, ChartControl)
+            Dim hitInfo As ChartHitInfo = chartControl.CalcHitInfo(e.GetPosition(chartControl))
+
+            Dim pointIndex As Integer = series.Points.IndexOf(hitInfo.SeriesPoint)
+            Dim point As SeriesPoint = hitInfo.SeriesPoint
+
+            Dim oldPointPercent As Double=0
+            CalcPointPercent(pointIndex, oldPointPercent)
+            point.Value += 1
+
+            Dim newPointPercent As Double = 0
+            CalcPointPercent(pointIndex, newPointPercent)
+
+            series.Rotation += 360 * (oldPointPercent - newPointPercent)
+            If series.Rotation > 360 Then
+                series.Rotation = series.Rotation Mod 360
+            End If
+        End Sub
+        Private Sub CalcPointPercent(ByVal pointIndex As Integer, ByRef oldPointPercent As Double)
+            For i As Integer = 0 To pointIndex - 1
+                oldPointPercent += series.Points(i).Value
+            Next i
+            oldPointPercent /= series.Points.Sum(Function(p) p.Value)
+        End Sub
+    End Class
+End Namespace
